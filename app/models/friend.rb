916ebd6a084781bb9f6cb4d6d132bea4f2e2c749
FuminoriArimoto <arimoto@queserser.co.jp>
2020-03-15 11:54:44 +0900

    バリデーションを追加した。

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,6 @@
 class Friend < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :name, length: { maximum: 10 }
+    validates :address, {presence: true, format: { with: VALID_EMAIL_REGEX }}
 end
